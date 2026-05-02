c5edd833e6ed4f97c87ff1fe1f9a0ceab10d1715
Karim Mohamed <kreemmohamed736@gmail.com>
2026-05-02 03:28:33 +0300

    Some Edit in Routes

--- routes/api.php
+++ routes/api.php
@@ -2,44 +2,55 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Category\CategoryController;
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\Products\ProductController;
 use App\Http\Controllers\Products\ProductsAttributesController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::controller(AuthController::class)->group(function(){
+// Auth routes
+Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
 });
 
-Route::middleware('auth:api')->controller(AuthController::class)->group(function(){
+Route::middleware('auth:api')->controller(AuthController::class)->group(function () {
     Route::post('/logout', 'logout');
     Route::get('/profile', 'profile');
 });
+//------------------   End Auth routes ---------------------------------
 
-Route::middleware('auth:api')->controller(CategoryController::class)->group(function(){
+
+// Category routes
+Route::controller(CategoryController::class)->group(function () {
+
     Route::get('/categories/tree', 'catTree');
-    Route::get('/categories', 'getAllCategories');
-    Route::post('create/category', 'createCategory');
     Route::get('show/category/{category}', 'show');
+    Route::get('/categories', 'getAllCategories');
+});
+Route::middleware('auth:api')->controller(CategoryController::class)->group(function () {
+    Route::post('create/category', 'createCategory');
     Route::put('update/category/{category}', 'update');
     Route::delete('delete/category/{category}', 'destroy');
 });
+//------------------   End Category routes ---------------------------------
 
-Route::middleware('auth:api')->controller(ProductController::class)->group(function(){
+
+// Product routes
+Route::middleware('auth:api')->controller(ProductController::class)->group(function () {
     Route::post('/create/product', 'createProduct');
     Route::put('/update/product/{product}', 'update');
     Route::delete('/delete/product/{product}', 'destroy');
 });
 
-Route::controller(ProductController::class)->group(function(){
+Route::controller(ProductController::class)->group(function () {
     Route::get('/products', 'getAllProducts');
     Route::get('/show/product/{product}', 'show');
 });
+//------------------   End Product routes ---------------------------------
 
 
-Route::controller(ProductsAttributesController::class)->group(function(){
+// Product Attributes routes
+Route::controller(ProductsAttributesController::class)->group(function () {
     Route::get('/attributes', 'getAllAttributes');
     Route::post('/create/attribute', 'createAttribute')->middleware('auth:api');
 });
+//------------------   End Product Attributes routes ---------------------------------
